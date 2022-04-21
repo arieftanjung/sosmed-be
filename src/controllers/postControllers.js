04a84b2f257c71893899a5ec9fadee1403c872c8
AriefTanjung <artayasmi@gmail.com>
2022-04-21 22:36:19 +0700

    update

---
+++ src/controllers/postControllers.js
@@ -0,0 +1,9 @@
+// // const { dbCon } = require("../connection");
+// // const fs = require("fs");
+
+// // module.exports = {
+// //     createPostingan: async (req,res) => {
+// //         let path = "/images"
+// //         const data
+// //     }
+// }
